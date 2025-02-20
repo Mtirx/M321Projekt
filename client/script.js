ada98979ac2bf1bf50a14d70550e851c3c53529d
Mtirx <marco.birchler8@gmail.com>
2025-02-20 13:07:07 +0100

    Frotend

---
+++ client/script.js
@@ -0,0 +1,44 @@
+const socket = new WebSocket("ws://localhost:3000");
+
+socket.addEventListener("open", (event) => {
+  console.log("WebSocket verbunden.");
+  const user = { id: 1, name: "John Doe" };
+  const message = {
+    type: "user",
+    user,
+  };
+  socket.send(JSON.stringify(message));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btnSendMessage = document.getElementById("btnSendMessage");
+  const messageInput = document.getElementById("messageInput");
+
+  btnSendMessage.addEventListener("click", () => {
+    const text = messageInput.value.trim();
+
+    if (text) {
+      const message = {
+        type: "message",
+        text: text,
+      };
+      socket.send(JSON.stringify(message));
+
+      messageInput.value = "";
+    }
+  });
+
+  messageInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      btnSendMessage.click();
+    }
+  });
+});
+
+socket.addEventListener("close", (event) => {
+  console.log("WebSocket geschlossen.");
+});
+
+socket.addEventListener("error", (event) => {
+  console.error("WebSocket Fehler:", event);
+});
